3e75e8b00f9b089a0cd39b4a70f354aae05cb69d
zino <115655149+Alvadus@users.noreply.github.com>
2024-11-03 11:54:07 +0100

    Update game_baldursgate3.py

--- game_baldursgate3.py
+++ game_baldursgate3.py
@@ -15,7 +15,7 @@ from .baldursgate3 import ModSettingsHelper
 class BaldursGate3Game(BasicGame, mobase.IPluginFileMapper):
     Name = "Baldur's Gate 3 Unofficial Support Plugin"
     Author = "chazwarp923 & Dragozino"
-    Version = "2.4.0"
+    Version = "2.4.5"
 
     GameName = "Baldur's Gate 3"
     GameShortName = "baldursgate3"
